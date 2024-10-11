74c5766c853fc92a114d69ed8f54d1fc1c331dfb
bertrava <vadim.bertrand@univ-grenoble-alpes.fr>
2024-10-11 09:48:41 +0000

    update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -8,3 +8,4 @@ conda activate cyclogeostrophy_impact_experiment
 pip install --upgrade pip
 conda install pyinterp
 pip install -r requirements.txt --upgrade
+# pip install -U "jax[cuda12]"  # uncomment this to install a cuda version of JAX
