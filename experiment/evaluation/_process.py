fb29d9194ba3f2d730cfa35db636881b0cf049f7
Vadim Bertrand <vadim.bertrand@univ-grenoble-alpes.fr>
2024-12-19 10:49:30 +0100

    updates to only evaluate the loss and its grads

--- experiment/evaluation/_process.py
+++ experiment/evaluation/_process.py
@@ -3,22 +3,16 @@ import math
 import os
 from typing import Dict, List, Tuple
 
-import clouddrift as cd
 import jax.numpy as jnp
 import numpy as np
 from jaxparrow.tools.geometry import compute_coriolis_factor, compute_spatial_step
 from jaxtyping import Array, Float
 import xarray as xr
 
-from ..io.drifter import DrifterData
 from ..io.experiment import ExperimentData
 from ..io.ssh import SSHData
 from ..logger._logger import LOGGER
-from .comparison import compare_methods
-from .interpolation import interpolate_drifters_location
-from .kinematics import compute_kinematics
 from .loss import compute_loss_value_and_grad
-from .metrics import compute_along_traj_metrics, compute_binned_metrics
 
 
 def _estimate_batch_indices(n_time: int, n_lat: int, n_lon: int, memory_per_device: float) -> List[int]:
@@ -31,23 +25,11 @@ def _estimate_batch_indices(n_time: int, n_lat: int, n_lon: int, memory_per_devi
     return indices.tolist()
 
 
-def _add_ssh(kinematics_ds: xr.Dataset, ssh_ds: xr.Dataset) -> xr.Dataset:
-    kinematics_ds["adt"] = (
-        ["latitude", "longitude"],
-        ssh_ds.adt.mean(dim="time").values,
-        {"what": ssh_ds.adt.attrs["long_name"], "units": "$m$"}
-    )
-    return kinematics_ds
-
-
 def estimate_and_evaluate(
     experiment_data: ExperimentData,
     experiment_config: str,
-    drifter_data: DrifterData,
     ssh_data: SSHData,
     cyclogeostrophy_fun: Callable,
-    bin_size: int,
-    save_all_times: bool,
     memory_per_device: int
 ) -> Tuple[xr.Dataset, xr.Dataset]:
     ssh_ds = ssh_data.dataset
@@ -61,71 +43,15 @@ def estimate_and_evaluate(
     batch_indices = [0] + batch_indices + [n_time]
 
     # apply per batch
-    errors_sum_ds, errors_count_ds, kinematics_sum_ds, kinematics_count_ds, mask = None, None, None, None, None
     for idx0, idx1 in zip(batch_indices[:-1], batch_indices[1:]):
         LOGGER.debug(f"2.i.0. mini-batch {[idx0, idx1]}")
-        errors_sum_ds_batch, errors_count_ds_batch, kinematics_sum_ds_batch, kinematics_count_ds_batch, mask_batch = (
-            process_batch(
-                idx0, idx1,
-                drifter_data.dataset, ssh_ds,
-                cyclogeostrophy_fun,
-                lat_t, lon_t,
-                bin_size,
-                save_all_times,
-                experiment_data, experiment_config
-            )
+        process_batch(
+            idx0, idx1,
+            ssh_ds,
+            cyclogeostrophy_fun,
+            lat_t, lon_t,
+            experiment_data, experiment_config
         )
-
-        if errors_sum_ds_batch is not None:
-            if errors_sum_ds is None:
-                errors_sum_ds = errors_sum_ds_batch
-            else:
-                errors_sum_ds += errors_sum_ds_batch
-
-        if errors_count_ds_batch is not None:
-            if errors_count_ds is None:
-                errors_count_ds = errors_count_ds_batch
-            else:
-                errors_count_ds += errors_count_ds_batch
-
-        if kinematics_sum_ds is None:
-            kinematics_sum_ds = kinematics_sum_ds_batch
-        else:
-            kinematics_sum_ds += kinematics_sum_ds_batch
-
-        if kinematics_count_ds is None:
-            kinematics_count_ds = kinematics_count_ds_batch
-        else:
-            kinematics_count_ds += kinematics_count_ds_batch
-
-        if mask is None:
-            mask = mask_batch
-        else:
-            mask |= mask_batch
-
-    LOGGER.info("2.N.1. Normalizing batched datasets")
-    with xr.set_options(keep_attrs=True):
-        errors_ds = errors_sum_ds / errors_count_ds
-    errors_ds["obs_density"] = errors_count_ds.to_array().mean(dim="variable")
-    errors_ds["obs_density"].attrs = {"what": "Drifter observations frequency"}
-    del errors_sum_ds, errors_count_ds
-    with xr.set_options(keep_attrs=True):
-        kinematics_ds = kinematics_sum_ds / kinematics_count_ds
-    del kinematics_sum_ds, kinematics_count_ds
-
-    LOGGER.info("2.N.2. Comparing methods")
-    errors_ds, kinematics_ds = compare_methods(errors_ds, kinematics_ds)
-
-    # LOGGER.info("2.N.3. Adding mean SSH")
-    # kinematics_ds = _add_ssh(kinematics_ds, ssh_ds)
-
-    LOGGER.info("2.N.4. Applying spatial mask")
-    kinematics_ds = kinematics_ds.where(~mask)
-
-    errors_ds.attrs["experiment_config"] = experiment_config
-    kinematics_ds.attrs["experiment_config"] = experiment_config
-
-    return errors_ds, kinematics_ds
 
 
 def _estimate_ssc(
@@ -215,13 +141,10 @@ def _save_all_times_dataset(
 def process_batch(
     idx0: int,
     idx1: int,
-    drifter_ds: xr.Dataset,
     ssh_ds: xr.Dataset,
     cyclogeostrophy_fun: Callable,
     lat_t: Float[Array, "lat lon"],
     lon_t: Float[Array, "lat lon"],
-    bin_size: int,
-    save_all_times: bool,
     experiment_data: ExperimentData,
     experiment_config: str
 ) -> Tuple[xr.Dataset, xr.Dataset, xr.Dataset, xr.Dataset, np.ndarray]:
@@ -240,58 +163,15 @@ def process_batch(
     loss_vars = compute_loss_value_and_grad(
         uv_fields, dx_u, dx_v, dy_u, dy_v, coriolis_factor_u, coriolis_factor_v, mask
     )
-    del dx_u, dx_v, dy_u, dy_v, coriolis_factor_u, coriolis_factor_v
+    del dx_u, dx_v, dy_u, dy_v, coriolis_factor_u, coriolis_factor_v, uv_fields, uva_fields
 
-    LOGGER.info("2.i.3. Interpolating SSC velocities to drifters positions - mini-batch")
+    LOGGER.info("2.i.3. Storing in a dataset - mini-batch")
     time = ssh_ds.time[idx0:idx1].values
-    drifter_ds.time.load()
-    drifter_batch = cd.ragged.subset(
-        drifter_ds,
-        {"time": (time[0], time[-1])},
-        row_dim_name="traj"
-    )
-
-    if drifter_batch:
-        drifter_batch = drifter_batch.drop_vars(["rowsize", "id"])
-        drifter_batch = interpolate_drifters_location(drifter_batch, time, lat_v, lon_u, uv_fields)
-
-        LOGGER.info("2.i.4. Evaluating SSC against drifters velocities - mini-batch")
-        LOGGER.info("2.i.4.1. Computing along trajectories metrics - mini-batch")
-        traj_metrics = compute_along_traj_metrics(drifter_batch, uv_fields.keys())
-        del drifter_batch
-        LOGGER.info("2.i.4.2. Computing binned metrics - mini-batch")
-        errors_sum_ds, errors_count_ds = compute_binned_metrics(ssh_ds, traj_metrics, uv_fields.keys(), bin_size)
-        errors_sum_ds.attrs["experiment_config"] = experiment_config
-        errors_count_ds.attrs["experiment_config"] = experiment_config
-        del traj_metrics
-    else:
-        errors_sum_ds, errors_count_ds = None, None
-
-    LOGGER.info("2.i.5. Computing additional kinematics - mini-batch")
-    kinematics_vars = compute_kinematics(uv_fields, uva_fields, lat_u, lon_u, lat_v, lon_v, mask)
-    del uv_fields, uva_fields
-
-    LOGGER.info("2.i.6. Storing in a dataset - mini-batch")
-    kinematics_ds = _kinematics_to_dataset(kinematics_vars, time, lat_t, lon_t)
+    kinematics_ds = _kinematics_to_dataset({}, time, lat_t, lon_t)
     kinematics_ds = _loss_to_dataset(kinematics_ds, loss_vars)
     kinematics_ds = kinematics_ds.where(~mask)
-
-    # LOGGER.info("2.i.7. Comparing methods - mini-batch")
-    # _, kinematics_ds = compare_methods(None, kinematics_ds)
-    
     kinematics_ds.attrs["experiment_config"] = experiment_config
 
-    LOGGER.info("2.i.8. Saving datasets - mini-batch")
-    _save_all_times_dataset(errors_sum_ds, experiment_data, "errors_sum")
-    _save_all_times_dataset(errors_count_ds, experiment_data, "errors_count")
-    if save_all_times:
-        _save_all_times_dataset(kinematics_ds, experiment_data, "kinematics", append=True)
-
-    LOGGER.info("2.i.9. Summing along time - mini-batch")
-    with xr.set_options(keep_attrs=True):
-        kinematics_sum_ds = kinematics_ds.sum(dim="time", skipna=True, keep_attrs=True)
-    kinematics_count_ds = xr.apply_ufunc(np.isfinite, kinematics_ds).sum(dim="time")
-    mask = np.any(mask, axis=0)
-    del kinematics_ds
-
-    return errors_sum_ds, errors_count_ds, kinematics_sum_ds, kinematics_count_ds, mask
+    LOGGER.info("2.i.4. Saving datasets - mini-batch")
+    _save_all_times_dataset(kinematics_ds, experiment_data, "kinematics", append=True)
+    del kinematics_ds, mask
--- main.py
+++ main.py
@@ -8,19 +8,15 @@ from hydra.core.hydra_config import HydraConfig
 from hydra.conf import HydraConf, JobConf
 from hydra.experimental.callback import Callback
 from hydra_zen import store, zen, make_custom_builds_fn
-import xarray as xr
 
 from experiment.evaluation._process import estimate_and_evaluate
 from experiment.evaluation.visualization import plot_fields
-from experiment.io import drifter_store, experiment_store, ssh_store
-from experiment.io.drifter import DrifterData
+from experiment.io import experiment_store, ssh_store
 from experiment.io.experiment import ExperimentData
 from experiment.io.ssh import SSHData
 from experiment.logger._logger import LOGGER
 from experiment.methods import cyclogeostrophy_conf
 from experiment.preproc import (
-    drifter_preproc_store,
-    drifter_default_preproc_conf,
     ssh_preproc_store,
     ssh_lon_to_180_180_preproc_conf
 )
@@ -42,23 +38,16 @@ class EnvVar(Callback):
         "_self_",
         {"experiment_data": "local"},
         {"ssh_data": "local"},
-        {"drifter_data": "local"},
     ]
 )
 def assess_cyclogeostrophy_impact(
     experiment_data: ExperimentData,  # where and how experiment description and outputs are stored (i.e. local or s3)
     ssh_data: SSHData,  # input SSH data
-    drifter_data: DrifterData,  # input drifter data
     ssh_rename: Dict[str, str] | None = None,  # dictionary mapping SSH dataset original to new variables names
-    drifter_rename: Dict[str, str] | None = None,  # dictionary mapping drifter dataset original to new variables names
     ssh_preproc: Callable = ssh_lon_to_180_180_preproc_conf,  # preprocessing applied to the drifters
-    drifter_preproc: Callable = drifter_default_preproc_conf,  # preprocessing applied to the drifters
     spatial_extent: Tuple[float, float, float, float] | None = None,  # spatial domain bounding box ([lon0, lon1, lat0, lat1])
     temporal_extent: Tuple[str, str] | None = None,  # temporal domain window
     cyclogeostrophy_fun: Callable = cyclogeostrophy_conf,  # callable for the cyclogeostrophy, with parameters set
-    bin_size: float = 1.,  # bins size for the errors computed vs. the drifters data (in °)
-    save_all_times: bool = True,  # whether to save variables at each time step
-    plot_all_times: bool = True,  # whether to plot variables at each time step
     memory_per_device: int = 35,  # available VRAM or RAM (in Gb)
     logger_level: int = logging.DEBUG  # logger outputs level
 ):
@@ -78,17 +67,11 @@ def assess_cyclogeostrophy_impact(
             outputs are saved. It can be a local filesystem, but also a s3 bucket.
         ssh_data (SSHData): Object representing the input SSH data. It can be only a path to the data file(s) on a
             local filesystem, but it can also describe an URL, or a s3 bucket.
-        drifter_data (DrifterData): Object representing the input drifter data. It can be only a path to the data
-            file(s) on a local filesystem, but it can also describe an URL, or a s3 bucket.
         ssh_rename (dict, optional): Dictionary mapping SSH dataset original to new variables names.
-            Defaults to None, which means no variables will be renamed.
-        drifter_rename (dict, optional): Dictionary mapping drifter dataset original to new variables names.
             Defaults to None, which means no variables will be renamed.
         ssh_preproc (Callable, optional): Function(s) applied to preprocess the SSH data.
             Defaults to ssh_lon_to_180_180_preproc_conf, which means longitudes are converted from [0, 360] to
             [-180, 180].
-        drifter_preproc (Callable, optional): Function(s) applied to preprocess the drifter data.
-            Defaults to drifter_default_preproc_conf, which means default preprocessing is applied.
         spatial_extent (list[float, float, float, float], optional): Spatial domain bounding box as
             [lon0, lon1, lat0, lat1].
             Defaults to None, which means no spatial restriction.
@@ -97,12 +80,6 @@ def assess_cyclogeostrophy_impact(
         cyclogeostrophy_fun (Callable, optional): Function for cyclogeostrophy computation with predefined parameters.
             Defaults to `cyclogeostrophy_conf`, which implies the use of the default `jaxparrow.cyclogeostrophy`
             parameters.
-        bin_size (int, optional): Bin size in degrees (°) for error computations versus drifter data.
-            Defaults to 1°.
-        save_all_times (bool, optional): Whether to save variables at each time step.
-            Defaults to True.
-        plot_all_times (bool, optional): Whether to plot variables at each time step. Requires `save_all_times=True`.
-            Defaults to True.
         memory_per_device (int, optional): Available VRAM or RAM per device in gigabytes (GB).
             Defaults to 35 GB.
         logger_level (int, optional): Logging output level (e.g., `logging.DEBUG`, `logging.INFO`, etc...).
@@ -130,11 +107,11 @@ def assess_cyclogeostrophy_impact(
         experiment_config = f.read()
 
     LOGGER.info("1. Loading input datasets")
-    LOGGER.info("1.1.1. Loading SSH")
+    LOGGER.info("1.1. Loading SSH")
     ssh_data.open_dataset(ssh_rename)
-    LOGGER.info("1.1.2. Preprocessing SSH")
+    LOGGER.info("1.2. Preprocessing SSH")
     ssh_data.apply_preproc(ssh_preproc)
-    LOGGER.info("1.1.2. Applying SSH extent")
+    LOGGER.info("1.3. Applying SSH extent")
     LOGGER.debug(
         f"before applying extent: "
         f"SSH domain dimensions: {ssh_data.dataset.dims}"
@@ -144,50 +121,14 @@ def assess_cyclogeostrophy_impact(
         f"after applying extent: "
         f"SSH domain dimensions: {ssh_data.dataset.dims}"
     )
-    LOGGER.info("1.2.1. Loading drifters")
-    drifter_data.open_dataset(drifter_rename, ssh_data)
-    LOGGER.info("1.2.2. Preprocessing drifters")
-    LOGGER.debug(
-        f"before preprocessing: "
-        f"{int(drifter_data.dataset.traj.size)} drifters & "
-        f"{int(drifter_data.dataset.obs.size)} observations"
-    )
-    drifter_data.apply_preproc(drifter_preproc)
-    LOGGER.debug(
-        f"after preprocessing: "
-        f"{int(drifter_data.dataset.traj.size)} drifters & "
-        f"{int(drifter_data.dataset.obs.size)} observations"
-    )
 
     LOGGER.info("2. Estimating and evaluating SSC methods in mini-batch. This may take a while...")
-    errors_ds, kinematics_ds = estimate_and_evaluate(
+    estimate_and_evaluate(
         experiment_data, experiment_config,
-        drifter_data, ssh_data,
+        ssh_data,
         cyclogeostrophy_fun,
-        bin_size,
-        save_all_times,
         memory_per_device
     )
-
-    LOGGER.info("3. Saving time averaged datasets")
-    errors_ds_path = os.path.join(
-        experiment_data.experiment_path, experiment_data.results_dir, "time_averaged_errors.zarr"
-    )
-    errors_ds.to_zarr(experiment_data.filesystem.get_path(errors_ds_path))  # noqa
-    kinematics_ds_path = os.path.join(
-        experiment_data.experiment_path, experiment_data.results_dir, "time_averaged_kinematics.zarr"
-    )
-    kinematics_ds.to_zarr(experiment_data.filesystem.get_path(kinematics_ds_path))  # noqa
-
-    LOGGER.info("4. Producing plots")
-    plot_fields(errors_ds, kinematics_ds, experiment_data)
-    if save_all_times and plot_all_times:
-        LOGGER.info("4.1. Producing plots for all time steps. This may take a while...")
-        kinematics_ds_path = os.path.join(
-            experiment_data.experiment_path, experiment_data.results_dir, "all_times_kinematics.zarr"
-        )
-        kinematics_ds = xr.open_zarr(experiment_data.filesystem.get_path(kinematics_ds_path))
-        plot_fields(None, kinematics_ds, experiment_data, all_times=True)
 
 
 if __name__ == "__main__":
