16df649e5dd023c125e0eacb9550ab1437c94b23
Vadim Bertrand <vadim.bertrand@univ-grenoble-alpes.fr>
2025-01-29 17:17:58 +0100

    add scripts to run experiments using neurost data

---
+++ scripts/neurost-global.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script runs the cyclogeostrophy impact experiment on NeurOST data, globally.
+# Saving all the daily snapshot datasets requires around 2TB of disk space.
+
+python main.py \
+    ssh_data=local \
+    ssh_data.resource.path='/summer/meom/workdir/bertrava/NEUROST_SSH-SST_L4_V2024.0.zarr' \
+    drifter_data=local \
+    drifter_data.resource.path='https://minio.dive.edito.eu/oidc-bertrava/data/gdp6h.zarr' \
+    temporal_extent='["2010", "2021"]'
