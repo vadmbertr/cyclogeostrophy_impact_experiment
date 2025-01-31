5c709256e225cd829d2c3c251a79970b3cfd2faf
Vadim Bertrand <vadim.bertrand@univ-grenoble-alpes.fr>
2025-01-31 15:28:22 +0100

    fix ssh preproc

--- experiment/preproc/ssh.py
+++ experiment/preproc/ssh.py
@@ -1,6 +1,17 @@
 import xarray as xr
 
 
+DEFAULT_STEPS = (
+    "lon_to_180_180",
+    "time_lat_lon"
+)
+
+
 def lon_to_180_180(ds: xr.Dataset) -> xr.Dataset:
-    ds["longitude"] = (ds["longitude"] + 180) % 360 - 180
+    ds["longitude"] = (ds.longitude + 180) % 360 - 180
+    ds = ds.sortby(ds.longitude)
     return ds
+
+
+def time_lat_lon(ds):
+    return ds.transpose("time", "latitude", "longitude")
