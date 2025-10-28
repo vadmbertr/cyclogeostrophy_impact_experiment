5ace991f5a1eff65a52db09918c9296b2dbb7626
Vadim <Vadim.Bertrand@etu.univ-grenoble-alpes.fr>
2025-10-28 10:37:25 +0100

    up dep

--- dependencies.sh
+++ dependencies.sh
@@ -3,10 +3,11 @@
 eval "$(command conda "shell.bash" "hook" 2> /dev/null)"
 eval "$(command conda "shell.zsh" "hook" 2> /dev/null)"
 
-conda env create -f environment.yml
+# conda env create -f environment.yml
 conda activate cyclogeostrophy_impact_experiment
 conda install pyinterp -c conda-forge
 pip install --upgrade pip
 pip install -r requirements.txt --upgrade
 pip install -U numpy  # needed to overwrite copernicusmarine version
+pip install -U zarr  # needed to overwrite clouddrift version
 # pip install -U "jax[cuda12]"  # uncomment this to install a cuda version of JAX
